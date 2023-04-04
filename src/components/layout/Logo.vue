770504a71ae7d9c86ad53be16643a97bf28339e3
Валентин <astrotrain55@gmail.com>
2023-04-04 15:37:27 +0500

    init

---
+++ src/components/layout/Logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="logo">
+    <span>твой фотограф</span>
+    <span class="openhouse">Kira Sekira</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'the-logo',
+};
+</script>
+
+<style lang="stylus">
+@require '../../assets/colors.styl'
+
+.logo
+  display flex
+  flex-direction column
+  color $titleColor
+  text-align center
+  font-size 50px
+  font-family monospace
+  .openhouse
+    font-family Openhouse, sans-serif
+    font-size 70px
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import VueParticles from 'vue-particles';
+import MasonryWall from '@yeger/vue-masonry-wall';
+import App from './App.vue';
+import router from './router';
+import './assets/styles.styl';
+
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(VueParticles)
+  .use(MasonryWall)
+  .mount('#app')
