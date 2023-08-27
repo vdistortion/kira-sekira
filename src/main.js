12d3b2f0b7f87d8c21904970b0323e39591b35f2
Валентин <astrotrain55@gmail.com>
2023-08-27 13:44:49 +0500

    refactor

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import VueParticles from 'vue-particles';
-import MasonryWall from '@yeger/vue-masonry-wall';
-import App from './App.vue';
 import router from './router';
+import App from './App.vue';
 import './assets/styles.styl';
 
 createApp(App)
   .use(createPinia())
   .use(router)
-  .use(VueParticles)
-  .use(MasonryWall)
   .mount('#app');
