d3c404ff0deb959febe5fbe54ba40e7d38700d2b
Валентин <astrotrain55@gmail.com>
2024-06-12 22:36:09 +0500

    dynamic years of experience

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -16,4 +16,23 @@ export class HomePageComponent implements OnInit {
   ngOnInit() {
     document.title = 'Kira Sekira';
   }
+
+  get yearsCount() {
+    const word = this.declensionOfNouns(this.years, ['год', 'года', 'лет']);
+    return [this.years, word].join(' ');
+  }
+
+  get years() {
+    const today = new Date();
+    const date = new Date('2019-05-01');
+    const m = today.getMonth() - date.getMonth();
+    let years = today.getFullYear() - date.getFullYear();
+    if (m < 0 || (m === 0 && today.getDate() < date.getDate())) years--;
+    return years;
+  }
+
+  declensionOfNouns(number: number, titles: string[]) {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  }
 }
